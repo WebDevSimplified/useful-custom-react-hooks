7d11a8c8403aa1f4715aa8e2915f6002d35b09d6
Ankit Sanghvi <ankit.sanghavi87@gmail.com>
2021-11-10 19:55:29 +0530

    useDebounce comments

--- src/3-useDebounce/useDebounce.js
+++ src/3-useDebounce/useDebounce.js
@@ -2,7 +2,13 @@ import { useEffect } from "react"
 import useTimeout from "../2-useTimeout/useTimeout"
 
 export default function useDebounce(callback, delay, dependencies) {
+
+  // Getting functions to reset (i.e. re-initialize i.e. restart) the timeout-counter that will run the callback should the timeout-interval complete
   const { reset, clear } = useTimeout(callback, delay)
+  
+  // Reinitializing our timeout-counter when dependencies change or when reset function is trigerred
   useEffect(reset, [...dependencies, reset])
+
+  // Clearing out this debounce setup if our component is unmounted
   useEffect(clear, [])
 }
