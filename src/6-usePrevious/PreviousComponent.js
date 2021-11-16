a8031d583cc4f1c79cfaf1bb7acde4c8341d4b7d
Ankit Sanghvi <ankit.sanghavi87@gmail.com>
2021-11-17 00:34:35 +0530

    changes

--- src/6-usePrevious/PreviousComponent.js
+++ src/6-usePrevious/PreviousComponent.js
@@ -1,21 +1,21 @@
-import { useState } from "react"
-import usePrevious from "./usePrevious"
+  import { useState } from "react"
+  import usePrevious from "./usePrevious"
 
-export default function PreviousComponent() {
-  const [count, setCount] = useState(0)
-  const [name, setName] = useState("Kyle")
-  const previousCount = usePrevious(count)
+  export default function PreviousComponent() {
+    const [count, setCount] = useState(0)
+    const [name, setName] = useState("Kyle")
+    const previousCount = usePrevious(count)
 
-  return (
-    <div>
+    return (
       <div>
-        {count} - {previousCount}
+        <div>
+          {count} - {previousCount}
+        </div>
+        <div>{name}</div>
+        <button onClick={() => setCount(currentCount => currentCount + 1)}>
+          Increment
+        </button>
+        <button onClick={() => setName("John")}>Change Name</button>
       </div>
-      <div>{name}</div>
-      <button onClick={() => setCount(currentCount => currentCount + 1)}>
-        Increment
-      </button>
-      <button onClick={() => setName("John")}>Change Name</button>
-    </div>
-  )
-}
+    )
+  }
