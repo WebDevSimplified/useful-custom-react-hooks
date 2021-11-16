3efec6c48e25554c87a0486f70a754a158a2cd90
Ankit Sanghvi <ankit.sanghavi87@gmail.com>
2021-11-17 00:12:53 +0530

    comment for 7th hook

--- src/7-useStateWithHistory/useStateWithHistory.js
+++ src/7-useStateWithHistory/useStateWithHistory.js
@@ -1,44 +1,65 @@
 import { useCallback, useRef, useState } from "react"
 
+// Bunch of utility functions to perform on a state being managed as an array 
+// 1. Accesing different elements of said array using a pointer
+// 2. Modifying the array
+
+
 export default function useStateWithHistory(
   defaultValue,
   { capacity = 10 } = {}
 ) {
   const [value, setValue] = useState(defaultValue)
+  
+  // Stores the array -> independently managed wrt component rerender
   const historyRef = useRef([value])
+  
+  // Stores the pointer to the array -> independently managed wrt component rerender
   const pointerRef = useRef(0)
 
+  // useCallback is used to run the cached "function" itself when its dependency ("value") changes 
   const set = useCallback(
     v => {
+
+      // resolve value from rerender based on whether its a val or a function returning a val 
       const resolvedValue = typeof v === "function" ? v(value) : v
+
+      // If resolvedVal doesn't match the val at the current index in array, we remove all elements in array ahead of said index and append the resolvedValue instead
       if (historyRef.current[pointerRef.current] !== resolvedValue) {
         if (pointerRef.current < historyRef.current.length - 1) {
           historyRef.current.splice(pointerRef.current + 1)
         }
         historyRef.current.push(resolvedValue)
 
+        // When length of array exceeds capacity, we use FIFO policy to maintain size of cached array
         while (historyRef.current.length > capacity) {
           historyRef.current.shift()
         }
         pointerRef.current = historyRef.current.length - 1
       }
+
+      // Lastly, we set the "value" to the resolved value 
       setValue(resolvedValue)
     },
     [capacity, value]
   )
 
+
+  // Updates the array pointer and sets the value to be array elem at current index 
   const back = useCallback(() => {
     if (pointerRef.current <= 0) return
     pointerRef.current--
     setValue(historyRef.current[pointerRef.current])
   }, [])
 
+  // Updates the array pointer and sets the value to be array elem at current index 
   const forward = useCallback(() => {
     if (pointerRef.current >= historyRef.current.length - 1) return
     pointerRef.current++
     setValue(historyRef.current[pointerRef.current])
   }, [])
 
+  // Updates the array pointer and sets the value to be array elem at current index 
   const go = useCallback(index => {
     if (index < 0 || index > historyRef.current.length - 1) return
     pointerRef.current = index
