6fbda0424b9d547b75fc612eca6c3440010f37b4
Ankit Sanghvi <ankit.sanghavi87@gmail.com>
2021-11-17 00:26:06 +0530

    comments for 10th hook

--- src/10-useFetch/useFetch.js
+++ src/10-useFetch/useFetch.js
@@ -1,9 +1,12 @@
 import useAsync from "../9-useAsync/useAsync"
 
+
+// To set some default options for our fetchAPI call since most contentType is always of type JSON
 const DEFAULT_OPTIONS = {
   headers: { "Content-Type": "application/json" },
 }
 
+// Simply sets the callback to the useAsync() hook to be a "fetchAPI" call along with some dependencies
 export default function useFetch(url, options = {}, dependencies = []) {
   return useAsync(() => {
     return fetch(url, { ...DEFAULT_OPTIONS, ...options }).then(res => {
