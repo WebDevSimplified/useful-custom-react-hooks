87c3de2895da017f0c3a1cc2d2d7b9806b61913f
Ankit Sanghvi <ankit.sanghavi87@gmail.com>
2021-11-17 00:24:26 +0530

    commented code for useAsync

--- src/9-useAsync/AsyncComponent.js
+++ src/9-useAsync/AsyncComponent.js
@@ -2,6 +2,7 @@ import useAsync from "./useAsync"
 
 export default function AsyncComponent() {
   const { loading, error, value } = useAsync(() => {
+    // 3 states and their updated versions are returned while the promise is getting resolved
     return new Promise((resolve, reject) => {
       const success = false
       setTimeout(() => {
--- src/9-useAsync/useAsync.js
+++ src/9-useAsync/useAsync.js
@@ -5,16 +5,23 @@ export default function useAsync(callback, dependencies = []) {
   const [error, setError] = useState()
   const [value, setValue] = useState()
 
+  // Simply manage 3 different states and update them as per the results of a Promise's resolution
+  // Here, we define a callback
   const callbackMemoized = useCallback(() => {
     setLoading(true)
     setError(undefined)
     setValue(undefined)
     callback()
+      // ON SUCCESS -> Set the data from promise as "value"  
       .then(setValue)
+      // ON FAILURE -> Set the err from promise as "error"  
       .catch(setError)
+      // Irresp of fail or success, loading must stop after promise has ran
       .finally(() => setLoading(false))
+      // This function runs everytime some dependency changes
   }, dependencies)
 
+  // To run the callback function each time it itself changes i.e. when its dependencies change
   useEffect(() => {
     callbackMemoized()
   }, [callbackMemoized])
