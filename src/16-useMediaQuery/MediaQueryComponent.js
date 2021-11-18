87a4865b7a78bef1eaa05eddec4b1848f3e4c53f
Ankit Sanghvi <ankit.sanghavi87@gmail.com>
2021-11-18 22:54:46 +0530

    16th hook

--- src/16-useMediaQuery/MediaQueryComponent.js
+++ src/16-useMediaQuery/MediaQueryComponent.js
@@ -1,6 +1,7 @@
 import useMediaQuery from "./useMediaQuery"
 
 export default function MediaQueryComponent() {
+  // Boolean i.e. true when media query is satisfied
   const isLarge = useMediaQuery("(min-width: 200px)")
 
   return <div>Large: {isLarge.toString()}</div>
--- src/16-useMediaQuery/useMediaQuery.js
+++ src/16-useMediaQuery/useMediaQuery.js
@@ -5,12 +5,25 @@ export default function useMediaQuery(mediaQuery) {
   const [isMatch, setIsMatch] = useState(false)
   const [mediaQueryList, setMediaQueryList] = useState(null)
 
+
+  // About the window.matchMedia() method : https://www.w3schools.com/jsref/met_win_matchmedia.asp
+  // eg :
+  // if (window.matchMedia("(max-width: 700px)").matches) {
+  /* The viewport is less than, or equal to, 700 pixels wide */
+// } else {
+//   /* The viewport is greater than 700 pixels wide */
+// }
+
+
+// Runs once at the start. Once our mediaQueryList is set to window.matchMedia, whenever our window is "resize"d, window.matchMedia is re-evaluated and the value of mediaQueryList is "change"d to the new emitted object
   useEffect(() => {
     const list = window.matchMedia(mediaQuery)
     setMediaQueryList(list)
+    // If returned list object satisfies media query then it will have a non-null "matches" value 
     setIsMatch(list.matches)
   }, [mediaQuery])
 
+  // Each time mediaQueryList elem is "change"d we check whether the new value of window.matchMedia has a non-null 'matches' value or not to determine whether our media query was satisfied or not
   useEventListener("change", e => setIsMatch(e.matches), mediaQueryList)
 
   return isMatch
