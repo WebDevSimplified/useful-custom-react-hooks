9a1fcbbdf7d381ca0c0e138833ec15c49e301113
Ankit Sanghvi <ankit.sanghavi87@gmail.com>
2021-11-22 19:57:00 +0530

    21st hook

--- src/21-useClickOutside/useClickOutside.js
+++ src/21-useClickOutside/useClickOutside.js
@@ -1,6 +1,8 @@
 import useEventListener from "../13-useEventListener/useEventListener"
 
 export default function useClickOutside(ref, cb) {
+
+  // Simple hook consuming our useEventListener hook and runs a passed callback if we click outside our reference element
   useEventListener(
     "click",
     e => {
