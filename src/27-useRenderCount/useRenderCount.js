912f6e7f9018f83ab4778416a2805b7982cbd259
Ankit Sanghvi <ankit.sanghavi87@gmail.com>
2021-11-23 00:48:55 +0530

    27th hook commented

--- src/27-useRenderCount/useRenderCount.js
+++ src/27-useRenderCount/useRenderCount.js
@@ -2,6 +2,8 @@ import { useEffect, useRef } from "react"
 
 export default function useRenderCount() {
   const count = useRef(1)
+  // Since React.StrictMode wrapper in App.js double runs (renders) all its child components except for our useEffect() function
+  // Done to help developers ensure that no weird side effects are going on, help them find bugs
   useEffect(() => count.current++)
   return count.current
 }
